b4609a80577103410ac10751268387017dc80508
Eric Gibertson <eric_gilbertson@yahoo.com>
2018-12-09 17:39:50 -0800

    remove old code

--- src/plantings_tab.ref/RootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import { blockInfo } from './PlantingsReducers'
-
-const rootReducer = combineReducers({
-    blockInfo,
-})
-
-
-export default rootReducer;
